d7ed2a3ace6ace4a4d473e3a556bce8ad010c56e
Thinzar kyaw <85160582+Thinzar-Kyaw@users.noreply.github.com>
2023-12-09 20:12:21 +0630

    Add files via upload

---
+++ JS/type.js
@@ -0,0 +1,11 @@
+let a = 2;
+let obj = {
+    name: "TZK",
+    age: 29
+};
+console.log("Type of a", typeof (a));
+console.log("Type of a ", a);
+console.log("Type of obj", typeof (obj));
+console.log("Type of obj", obj);
+console.log("Type of Obj ", obj.name);
+console.log("Type of Obj ", obj.age);
